66eff36e22e5f4e499e536574aae7d59af927b3f
Neil Crawford <neil.crawford@gmail.com>
2015-10-03 21:20:28 +0100

    Takewhile is not working nicely, got the right answer too!

--- project-euler/problem-002/src/index.js
+++ project-euler/problem-002/src/index.js
@@ -1,9 +1,52 @@
 "use strict"
 
-module.exports.sumEvensLessThan = (count) => {
-  if(count == 100){
-    return 44;
-  }
+var _ = require('highland')
 
-  throw new Error("No implemented for $(count)")
+var fibonacciGenerator = function() {
+    let a = 1, b = 2
+    return (push, next) => {
+        push(null, a)
+        b = a + b;
+        a = b - a;
+        next()
+    }
+};
+
+var takeWhile = (f) => {
+  return (err, x, push, next) => {
+    if (err) {
+      // pass errors along the stream and consume next value
+      push(err);
+      next();
+    }
+    else if (x === _.nil) {
+      // pass nil (end event) along the stream
+      push(null, x);
+    }
+    else {
+      // pass on the value only if the value passes the predicate
+      if (f(x)) {
+        push(null, x);
+        next();
+      } else {
+        push(null, _.nil)
+      }
+    }
+  }
+}
+
+module.exports.sumEvensLessThan = (count, done) => {
+  _(fibonacciGenerator())
+    .consume(takeWhile((x) => {
+      return x < count
+    }))
+    .filter((item) => {
+      return item % 2 === 0
+    })
+    .reduce(0, (a , b) => {
+      return a + b
+    })
+    .toArray( (result) => {
+      done(null, result[0])
+    })
 }
--- project-euler/problem-002/test/index.spec.js
+++ project-euler/problem-002/test/index.spec.js
@@ -4,10 +4,33 @@ let should = require('should')
 let sut = require('./../src/index')
 
 describe( 'Fibonacci evens', () =>
-  describe( 'First 10 in sequence', () =>
-    it( 'should return 44 when the value is less than 100', () =>
-        sut.sumEvensLessThan(100)
-          .should.equal(44)
+  describe( 'First 10 in sequence', () => {
+    it( 'should return 44 when the value is less than 100', (done) => {
+        sut.sumEvensLessThan(100, (err,result) => {
+          result.should.equal(44)
+          done()
+        })
+    })
+
+    it( 'should return 2 when the value is less than 3', (done) =>
+        sut.sumEvensLessThan(3, (err,result) => {
+          result.should.equal(2)
+          done()
+        })
     )
-  )
+
+    it( 'should return 10 when the value is less than 9', (done) =>
+        sut.sumEvensLessThan(9, (err,result) => {
+          result.should.equal(10)
+          done()
+        })
+    )
+
+    it( 'should return 4613732 when the value is less than 4000000', (done) =>
+        sut.sumEvensLessThan(4000000, (err,result) => {
+          result.should.equal(4613732)
+          done()
+        })
+    )
+  })
 )
