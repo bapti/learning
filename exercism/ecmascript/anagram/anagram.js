735a85b95c7863db731a8daf24deb7760b64f5ec
Neil Crawford <neil.crawford@gmail.com>
2017-01-26 13:02:57 +0000

    Anagram is finished

--- exercism/ecmascript/anagram/anagram.js
+++ exercism/ecmascript/anagram/anagram.js
@@ -1,9 +1,50 @@
-class Anagram {
-  constructor() {
-    this.input = input
+const hashSet = (word) => {
+  return word
+    .toLowerCase()
+    .replace(/ /g, '')
+    .split('')
+    .reduce( (acc, letter) => {
+      !acc[letter] ? acc[letter] = 1 : acc[letter] += 1
+      return acc
+    },{})
+}
+
+const hashMatch = (hash1, hash2) => {
+  if(Object.keys(hash1).length !== Object.keys(hash2).length){
+    return false
   }
 
-  matches(args) {
+  for (var prop in hash1) {
+    if(hash2[prop] !== hash1[prop]){
+      return false
+    }
+  }
 
+  return true
+}
+
+const flatten = array => array.reduce(
+  (acc, item) => acc.concat(Array.isArray(item) ? flatten(item) : item), []
+);
+
+class Anagram {
+  constructor(input) {
+    this.inputWord = input.toLowerCase()
+    this.hashedInput = hashSet(input)
+  }
+
+  matches(...words) {
+
+    return flatten(words).reduce((acc, word) => {
+      if (word.toLowerCase() === this.inputWord) {
+        return acc
+      }
+      if (hashMatch(this.hashedInput, hashSet(word))) {
+        acc.push(word)
+      }
+      return acc
+    }, [])
   }
 }
+
+export default Anagram
