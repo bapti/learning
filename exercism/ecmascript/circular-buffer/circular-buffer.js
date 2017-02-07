e50546c44ff25a6f812e92394426e0149bda2c70
Neil Crawford <neil.crawford@gmail.com>
2017-02-07 21:56:12 +0000

    Adding complete solution to circular buffer

--- exercism/ecmascript/circular-buffer/circular-buffer.js
+++ exercism/ecmascript/circular-buffer/circular-buffer.js
@@ -1,28 +1,50 @@
+var data = []
+var maxSize = 0
 
-const bufferEmptyException = () => {
-  throw new Error("Buffer is empty")
-}
-const bufferFullException = () => {
-  throw new Error("Buffer is full")
-}
-// const xyz = (x) => () =>
+const bufferEmptyException = () => new Error('Buffer is empty')
+const bufferFullException = () => new Error('Buffer is full')
 
-const circularBuffer = (size) => {
+const checkBufferEmpty = fn => (...args) => {
+  if(data.length === 0){
+    throw bufferEmptyException()
+  }
+  return fn(...args)
+}
+
+const discardInvalid = fn => value => !value ? null : fn(value)
+
+
+const checkBufferFull = (fn) => (...args) => {
+  if(data.length >= maxSize){
+    throw bufferFullException()
+  }
+  return fn(...args)
+}
+
+const read = () => {
+  var result = data[0]
+  data = data.slice(1, data.length)
+  return result
+}
+
+const write = value => data.push(value)
+const clear = () => data = []
+
+const forceWrite = value => {
+  if(data.length >= maxSize){
+    read()
+  }
+  write(value)
+}
+
+const circularBuffer = size => {
+  clear()
+  maxSize = size
   return {
-    buffer: [],
-    read: (x) => {
-      console.log('hi');
-      if(buffer.length === 0){
-        bufferEmptyException()
-      }
-    },
-    write: (x) => {
-      console.log('hi');
-
-      if(buffer.length === size){
-        bufferFullException()
-      }
-    }
+    read: checkBufferEmpty(read),
+    write: checkBufferFull(discardInvalid(write)),
+    forceWrite,
+    clear
   }
 }
 
--- exercism/ecmascript/circular-buffer/circular-buffer.spec.js
+++ exercism/ecmascript/circular-buffer/circular-buffer.spec.js
@@ -9,14 +9,14 @@ describe('CircularBuffer', () => {
     expect(()=>buffer.read()).toThrow(bufferEmptyException());
   });
 
-  xit('write and read back one item', () => {
+  it('write and read back one item', () => {
     const buffer = circularBuffer(1);
     buffer.write('1');
     expect(buffer.read()).toBe('1');
     expect(()=>buffer.read()).toThrow(bufferEmptyException());
   });
 
-  xit('write and read back multiple items', () => {
+  it('write and read back multiple items', () => {
     const buffer = circularBuffer(2);
     buffer.write('1');
     buffer.write('2');
@@ -25,7 +25,7 @@ describe('CircularBuffer', () => {
     expect(()=>buffer.read()).toThrow(bufferEmptyException());
   });
 
-  xit('clearing a buffer', () => {
+  it('clearing a buffer', () => {
     const buffer = circularBuffer(2);
     buffer.write('1');
     buffer.write('2');
@@ -37,7 +37,7 @@ describe('CircularBuffer', () => {
     expect(buffer.read()).toBe('4');
   });
 
-  xit('alternate write and read', () => {
+  it('alternate write and read', () => {
     const buffer = circularBuffer(2);
     buffer.write('1');
     expect(buffer.read()).toBe('1');
@@ -45,7 +45,7 @@ describe('CircularBuffer', () => {
     expect(buffer.read()).toBe('2');
   });
 
-  xit('reads back oldest item', () => {
+  it('reads back oldest item', () => {
     const buffer = circularBuffer(3);
     buffer.write('1');
     buffer.write('2');
@@ -55,7 +55,7 @@ describe('CircularBuffer', () => {
     expect(buffer.read()).toBe('3');
   });
 
-  xit('writes of undefined or null don\'t occupy buffer', () => {
+  it('writes of undefined or null don\'t occupy buffer', () => {
     const buffer = circularBuffer(3);
     buffer.write(null);
     buffer.write(undefined);
@@ -63,14 +63,14 @@ describe('CircularBuffer', () => {
     expect(buffer.read()).toBe('1');
   });
 
-  xit('writing to a full buffer throws a BufferFullException', () => {
+  it('writing to a full buffer throws a BufferFullException', () => {
     const buffer = circularBuffer(2);
     buffer.write('1');
     buffer.write('2');
     expect(() => buffer.write('A')).toThrow(bufferFullException());
   });
 
-  xit('forced writes over write oldest item in a full buffer', () => {
+  it('forced writes over write oldest item in a full buffer', () => {
     const buffer = circularBuffer(2);
     buffer.write('1');
     buffer.write('2');
@@ -80,7 +80,7 @@ describe('CircularBuffer', () => {
     expect(()=>buffer.read()).toThrow(bufferEmptyException());
   });
 
-  xit('forced writes act like write in a non-full buffer', () => {
+  it('forced writes act like write in a non-full buffer', () => {
     const buffer = circularBuffer(2);
     buffer.write('1');
     buffer.forceWrite('2');
@@ -89,7 +89,7 @@ describe('CircularBuffer', () => {
     expect(()=>buffer.read()).toThrow(bufferEmptyException());
   });
 
-  xit('alternate force write and read into full buffer', () => {
+  it('alternate force write and read into full buffer', () => {
     const buffer = circularBuffer(5);
     [1,2,3].map(i => buffer.write(i.toString()));
     buffer.read();
