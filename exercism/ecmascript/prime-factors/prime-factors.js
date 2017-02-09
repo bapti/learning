b9ffb349fb428e5ca1fc89e27ad9b9cfba8524f0
Neil Crawford <neil.crawford@gmail.com>
2017-02-09 20:31:20 +0000

    Adding solution to prime factors

--- exercism/ecmascript/prime-factors/prime-factors.js
+++ exercism/ecmascript/prime-factors/prime-factors.js
@@ -1,6 +1,14 @@
+import prime from 'get-primes'
+
+const primeNumbers = prime(900000);
+
 class PrimeFactors {
-  for(n) {
-    return []
+  for(n, factors = []) {
+    if(n === 1) return factors.sort((a,b) => a > b)
+
+    let factor = primeNumbers.find(x => n % x === 0)
+
+    return this.for(n/factor, [...factors, factor])
   }
 }
 
