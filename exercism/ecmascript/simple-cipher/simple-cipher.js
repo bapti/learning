6d3bde69c4e024f1538092ba514dba3ddd99d312
Neil Crawford <neil.crawford@gmail.com>
2017-02-16 09:27:37 +0000

    Adding simple cipher solution

--- exercism/ecmascript/simple-cipher/simple-cipher.js
+++ exercism/ecmascript/simple-cipher/simple-cipher.js
@@ -3,34 +3,32 @@ const scores = {
   j: 9,  k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17,
   s: 18, t: 19, u: 20, v: 21, w: 22, x: 23, y: 24, z: 25
 }
+const getChar = (shift) =>
+  String.fromCharCode('a'.charCodeAt() + shift)
 
-const shiftChar = (char, cipher) => {
-  const diff = scores[char] - scores[cipher] <= 0
-    ? scores[char] + scores[cipher]
-    : scores[char] + scores[cipher] - 26
-  console.log(char, cipher);
-  const newChar = String.fromCharCode('a'.charCodeAt() + diff)
-  return newChar
-}
+const getShift = (char, offset, action) =>
+  action(scores[char], scores[offset]) % 26
 
-const encode = (key, text) => {
-  return text.split('')
-    .reduce((acc, char, i) => {
-      return [...acc, shiftChar(char, key[i])]
-    },[])
+const shiftChar = (char, offset, action) =>
+  getChar(getShift(char, offset, action))
+
+const convert = (key, text, action) =>
+  text.split('')
+    .map( (char, i) => shiftChar(char, key[i], action) )
     .join('')
-}
 
-const decode =  (key, text) => {
-  return text.split('')
-    .reduce((acc, char, i) => {
-      return [...acc, shiftChar(key[i], char)]
-    },[])
-    .join('')
+const encode = (key, text) => convert(key, text, (a, b) => a + b)
+const decode = (key, text) => convert(key, text, (a, b) => a - b)
+
+const validateSeed = (seed) => {
+  if( seed === undefined ) return 'abcdefghij'
+  if( seed === '' ) throw new Error('Bad key')
+  if( !seed.match(/^[a-z]+$/) ) throw new Error('Bad key')
+  return seed
 }
 
 export default (seed) => {
-  const key = !seed ? 'abcdefghij' : ''
+  const key = validateSeed(seed)
   return {
     key,
     encode: encode.bind(null, key),
