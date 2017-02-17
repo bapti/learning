03bd1d4c3a574804f772c35afe76e86517d4b53f
Neil Crawford <neil.crawford@gmail.com>
2017-02-17 16:27:44 +0000

    Starting pig latin exercise

---
+++ exercism/ecmascript/pig-latin/pig-latin.js
@@ -0,0 +1,19 @@
+const vowels = new Set([ 'a', 'e', 'i', 'o', 'u'])
+const consonants = new Set([
+  'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
+  'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
+])
+
+const isVowel = (letter) => vowels.has(letter)
+const isConsonant = (letter) => consonants.has(letter)
+
+const translateVowel
+
+const translate = (word) => {
+  let 
+  return
+}
+
+export default {
+  translate
+}
