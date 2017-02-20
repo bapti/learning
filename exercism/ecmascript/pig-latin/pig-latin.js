471a972bba91eff5210f5f4d5e9c582ab02577ea
Neil Crawford <neil.crawford@gmail.com>
2017-02-20 17:10:24 +0000

    Adding solution to pig latin

--- exercism/ecmascript/pig-latin/pig-latin.js
+++ exercism/ecmascript/pig-latin/pig-latin.js
@@ -1,19 +1,23 @@
-const vowels = new Set([ 'a', 'e', 'i', 'o', 'u'])
-const consonants = new Set([
-  'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
-  'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
-])
-
-const isVowel = (letter) => vowels.has(letter)
-const isConsonant = (letter) => consonants.has(letter)
-
-const translateVowel
-
-const translate = (word) => {
-  let 
-  return
+const firstVowel = word =>  {
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+  const vowel = word.split('').find(x => vowels.has(x))
+  return { vowel, index: word.indexOf(vowel) }
 }
 
-export default {
-  translate
+const breakWordIntoParts = word => {
+  let { vowel, index } = firstVowel(word)
+  if(index === 0) return [ word, '' ]
+  if(word[index-1] == 'q') index++
+  return [
+    word.substring(0, index),
+    word.substring(index, word.length)
+  ]
 }
+
+const translateWord = (word) =>
+  breakWordIntoParts(word).reverse().join('') + 'ay'
+
+const translate = (sentance) =>
+  sentance.split(' ').map(translateWord).join(' ')
+
+export default () => ({ translate })
--- exercism/ecmascript/pig-latin/pig-latin.spec.js
+++ exercism/ecmascript/pig-latin/pig-latin.spec.js
@@ -7,43 +7,43 @@ describe('Pig Latin', () => {
     expect(translator.translate('apple')).toEqual('appleay');
   });
 
-  xit('translates a word beginning with e', () => {
+  it('translates a word beginning with e', () => {
     expect(translator.translate('ear')).toEqual('earay');
   });
 
-  xit('translates a word beginning with p', () => {
+  it('translates a word beginning with p', () => {
     expect(translator.translate('pig')).toEqual('igpay');
   });
 
-  xit('translates a word beginning with k', () => {
+  it('translates a word beginning with k', () => {
     expect(translator.translate('koala')).toEqual('oalakay');
   });
 
-  xit('translates a word beginning with ch', () => {
+  it('translates a word beginning with ch', () => {
     expect(translator.translate('chair')).toEqual('airchay');
   });
 
-  xit('translates a word beginning with qu', () => {
+  it('translates a word beginning with qu', () => {
     expect(translator.translate('queen')).toEqual('eenquay');
   });
 
-  xit('translates a word with a consonant preceding qu', () => {
+  it('translates a word with a consonant preceding qu', () => {
     expect(translator.translate('square')).toEqual('aresquay');
   });
 
-  xit('translates a word beginning with th', () => {
+  it('translates a word beginning with th', () => {
     expect(translator.translate('therapy')).toEqual('erapythay');
   });
 
-  xit('translates a word beginning with thr', () => {
+  it('translates a word beginning with thr', () => {
     expect(translator.translate('thrush')).toEqual('ushthray');
   });
 
-  xit('translates a word beginning with sch', () => {
+  it('translates a word beginning with sch', () => {
     expect(translator.translate('school')).toEqual('oolschay');
   });
 
-  xit('translates a phrase', () => {
+  it('translates a phrase', () => {
     expect(translator.translate('quick fast run'))
       .toEqual('ickquay astfay unray');
   });
