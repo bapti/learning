d993c2e9a6248107b5759d5f5e88cae8a05991d8
Neil Crawford <neil.crawford@gmail.com>
2017-02-22 21:58:34 +0000

    Adding secret handshake exercise

---
+++ exercism/ecmascript/secret-handshake/secret-handshake.js
@@ -0,0 +1,24 @@
+const data = [
+  [ 0x1,  acc => [...acc, "wink"] ],
+  [ 0x2,  acc => [...acc, "double blink"] ],
+  [ 0x4,  acc => [...acc, "close your eyes"] ],
+  [ 0x8,  acc => [...acc, "jump"] ],
+  [ 0x10, acc => acc.reverse() ]
+]
+
+const commands = (hex) =>
+  data.reduce((acc, [mask, action]) => {
+    return mask & hex ? action(acc) : acc
+  }, [])
+
+
+const secretHandshake = (hex) => {
+  if(!Number.isInteger(hex)){
+    throw new Error('Handshake must be a number')
+  }
+  return {
+    commands: () => commands(hex)
+  }
+}
+
+export default secretHandshake
