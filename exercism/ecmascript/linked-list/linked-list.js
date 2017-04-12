e9d2ffba1699a8f3d59c31f3f5017f548eedc46b
Neil Crawford <neil.crawford@gmail.com>
2017-04-12 15:08:53 +0100

    Updating linked list

--- exercism/ecmascript/linked-list/linked-list.js
+++ exercism/ecmascript/linked-list/linked-list.js
@@ -1,18 +1,48 @@
-const linkedList = () => {
-  const data = []
+const insert = (prev, value, next) => {
+  prev.next = next.prev = { prev, value, next }
+}
 
-  return {
-    pop: () => data.pop(),
-    push: (x) => data.push(x),
-    shift: () => data.shift(),
-    unshift: (x) => data.unshift(x),
-    count: () => data.length,
-    delete: (n) => {
-      if(data.indexOf(n) !== -1) {
-        data.splice(data.indexOf(n), 1)
-      }
+const remove = ({prev, value, next}) => {
+  [prev.next, next.prev] = [next, prev]
+  return value
+}
+
+function * elements (list) {
+  for (let el = list; (el = el.next) !== list; yield el);
+}
+
+function reduce (list, fn, acc) {
+  for (var el of elements(list)) {
+    acc = fn(acc, el)
+  }
+  return acc
+}
+
+function find (list, value) {
+  for (var el of elements(list)) {
+    if (el.value === value) {
+      return el
     }
   }
 }
 
-export default linkedList
+const linkedList2 = () => {
+  const list = {}
+  list.prev = list.next = list
+
+  return Object.freeze({
+    push:     (value) => insert(list.prev, value, list),
+    unshift:  (value) => insert(list, value, list.next),
+    pop:      () => remove(list.prev),
+    shift:    () => remove(list.next),
+    count:    () => reduce(list, count => ++count, 0),
+    delete:   (value) => {
+                let element = find(list, value)
+                if(element) {
+                  remove(element)
+                }
+              }
+  })
+}
+
+export default linkedList2
