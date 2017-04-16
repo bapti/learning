ac5ee22fde56ed054da08d4eeb7e399f5d0515f8
Neil Crawford <neil.crawford@gmail.com>
2017-04-16 21:03:25 +0100

    Updated solution to be simpler

--- exercism/ecmascript/binary-search/binary-search.js
+++ exercism/ecmascript/binary-search/binary-search.js
@@ -2,28 +2,19 @@ const isSorted = (arr) => {
   return arr.every((n, i) => arr[i+1] ? n <= arr[i+1] : true)
 }
 
-const indexOf = (data) => (n) => {
-  return bisect(data, n, 0, data.length-1, Math.floor)
-}
-
-const bisect = (data, searchN, lowIndex, highIndex, roundingFn) => {
-  const midpointIndex = roundingFn((highIndex+lowIndex)/2)
+const bisect = (data, n, lowIndex = 0, highIndex = data.length) => {
+  const midpointIndex = Math.floor((highIndex+lowIndex)/2)
   const candidate = data[midpointIndex]
-  if(searchN === candidate) return midpointIndex
+  if(n === candidate) return midpointIndex
   if(midpointIndex === lowIndex || midpointIndex === highIndex) return -1
-  return bisect(
-    data,
-    searchN,
-    searchN > candidate ? midpointIndex : lowIndex,
-    searchN > candidate ? highIndex : midpointIndex,
-    searchN < candidate ? Math.floor : Math.ceil
-  )
+  if(n > candidate) return bisect(data, n, midpointIndex, highIndex)
+  return bisect(data, n, lowIndex, midpointIndex)
 }
 
 class BinarySearch {
   constructor(data){
     this.array = isSorted(data) ? data : undefined
-    this.indexOf = indexOf(this.array)
+    this.indexOf = (n) => bisect(data, n)
   }
 }
 
