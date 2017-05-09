c40711430fddd7043d54abda2c3dc46773740667
Neil Crawford <neil.crawford@gmail.com>
2017-05-09 10:19:08 +0100

    Refactoring saddle points exercise to be more succinct

--- exercism/ecmascript/saddle-points/saddle-points.js
+++ exercism/ecmascript/saddle-points/saddle-points.js
@@ -1,41 +1,22 @@
-function * allCoordinates(rowsMax, colsMax) {
-  for(let row = 0; row < rowsMax; row++) {
-    for(let col = 0; col < colsMax; col++) {
-      yield [row, col]
+function * saddles(rows, columns) {
+  for(let r = 0; r < rows.length; r++) {
+    for(let c = 0; c < columns.length; c++) {
+      if(isSaddlePoint(rows[r][c], rows[r], columns[c])) {
+        yield [r, c]
+      }
     }
   }
 }
 
-const getSaddlePoints = (rows, columns) => {
-  return [...allCoordinates(rows.length, columns.length)]
-    .reduce((acc, [rowIndex, colIndex]) => {
-      const result = isSaddlePoint(rowIndex, colIndex, rows, columns)
-      return result ? [...acc, [rowIndex, colIndex]] : acc
-    }, [])
-}
-
-const isSaddlePoint = (rowIndex, colIndex, rows, columns) => {
-  return isGreatestInRow(colIndex, rows[rowIndex])
-    && isLeastInColumn(rowIndex, columns[colIndex])
-}
-
-const isGreatestInRow = (colIndex, row) =>
-  row.every(n => row[colIndex] >= n)
-
-const isLeastInColumn = (rowIndex, column) =>
-  column.every(n => column[rowIndex] <= n )
+const isSaddlePoint = (value, row, column) =>
+  Math.max(...row) === value && Math.min(...column) === value
 
 const range = (n) => [...Array(n).keys()]
 
 const matrix = (stringMatrix) => {
-  const rows = stringMatrix
-    .split("\n")
-    .map(row => row.split(" ").map(n => Number(n)))
-
-  const columns = range(rows.length)
-    .map(i => rows.map(row => row[i]))
-
-  const saddlePoints = getSaddlePoints(rows, columns)
+  const rows = stringMatrix.split("\n").map(row => row.split(" ").map(Number))
+  const columns = range(rows.length).map(i => rows.map(row => row[i]))
+  const saddlePoints = [...saddles(rows, columns)]
 
   return { rows, columns, saddlePoints }
 }
