ba485322d0dc2350f68289a8631d123c720fd46b
Neil Crawford <neil.crawford@gmail.com>
2017-05-24 09:27:41 +0100

    Adding final solution to meetup exercise

--- exercism/ecmascript/meetup/meetup.js
+++ exercism/ecmascript/meetup/meetup.js
@@ -4,23 +4,29 @@ const offsetBetweenDays = (day1, day2) => {
     : day2 - day1
 }
 
+const getDayAsNum = (day) => [
+  "Monday", "Tuesday", "Wednesday", "Thursday",
+  "Friday", "Saturday", "Sunday"
+].indexOf(day) + 1
+
 const offsetDayForWeek = weeks => (y, m, d) => {
   const firstDayOfMonth = (new Date(y, m, 1)).getDay()
-  const indexOfDay = days.indexOf(d) + 1
+  const indexOfDay = getDayAsNum(d)
   return 1 + offsetBetweenDays(firstDayOfMonth, indexOfDay) + weeks * 7
 }
 
-const lastDayOfMonth = (y, m, d) => {
+const calculateLast = (y, m, d) => {
   const lastDayOfMonth = new Date(y, m+1, 0)
   const dayOflastDayOfMonth = lastDayOfMonth.getDay()
-  const indexOfDay = days.indexOf(d) + 1
-  return lastDayOfMonth.getDate() - offsetBetweenDays(dayOflastDayOfMonth, indexOfDay)
+  const indexOfDay = getDayAsNum(d)
+  return lastDayOfMonth.getDate() - offsetBetweenDays(indexOfDay, dayOflastDayOfMonth)
 }
 
-const days = [
-  "Monday", "Tuesday", "Wednesday", "Thursday",
-  "Friday", "Saturday", "Sunday"
-]
+const calculateTeenth = (y, m, d) => {
+  const firstTeenthOfMonth = (new Date(y, m, 13)).getDay()
+  const indexOfDay = getDayAsNum(d)
+  return 13 + offsetBetweenDays(firstTeenthOfMonth, indexOfDay)
+}
 
 const occurrences = {
   '1st': offsetDayForWeek(0),
@@ -28,16 +34,17 @@ const occurrences = {
   '3rd': offsetDayForWeek(2),
   '4th': offsetDayForWeek(3),
   '5th': offsetDayForWeek(4),
-  'last': lastDayOfMonth
-  // 'teenth': (y, m, d) => adf,
+  'last': calculateLast,
+  'teenth': calculateTeenth
 }
-
-const teenths = [ 13, 14, 15, 16, 17, 18, 19 ]
-
 
 const meetup = (year, month, day, occurrence) => {
   const computedDay = occurrences[occurrence](year, month, day)
-  return new Date(year, month, computedDay)
+  const meetupDate = new Date(year, month, computedDay)
+  if(month !== meetupDate.getMonth()) {
+    throw 'Month out of bounds for meetup'
+  }
+  return meetupDate
 }
 
 export default meetup
--- exercism/ecmascript/meetup/meetup.spec.js
+++ exercism/ecmascript/meetup/meetup.spec.js
@@ -6,11 +6,11 @@ function MeetupDayException(message) {
 }
 
 describe('meetupDay()', () => {
-  xit('test monteenth of may 2013', () => {
+  it('test monteenth of may 2013', () => {
     expect(meetupDay(2013, 4, 'Monday', 'teenth')).toEqual(new Date(2013, 4, 13));
   });
 
-  xit('test saturteenth of february 2013', () => {
+  it('test saturteenth of february 2013', () => {
     expect(meetupDay(2013, 1, 'Saturday', 'teenth')).toEqual(new Date(2013, 1, 16));
   });
 
@@ -42,19 +42,19 @@ describe('meetupDay()', () => {
     expect(meetupDay(2014, 11, 'Wednesday', 'last')).toEqual(new Date(2014, 11, 31));
   });
 
-  fit('test last sunday of only four week february 2015', () => {
+  it('test last sunday of only four week february 2015', () => {
     expect(meetupDay(2015, 1, 'Sunday', 'last')).toEqual(new Date(2015, 1, 22));
   });
 
-  xit('test first friday of december 2012', () => {
+  it('test first friday of december 2012', () => {
     expect(meetupDay(2012, 11, 'Friday', '1st')).toEqual(new Date(2012, 11, 7));
   });
 
-  xit('test fifth monday of march 2015', () => {
+  it('test fifth monday of march 2015', () => {
     expect(meetupDay(2015, 2, 'Monday', '5th')).toEqual(new Date(2015, 2, 30));
   });
 
-  xit('test nonexistent fifth monday of february 2015', () => {
+  it('test nonexistent fifth monday of february 2015', () => {
     expect(() => {
       meetupDay(2015, 1, 'Monday', '5th');
     }).toThrow();
