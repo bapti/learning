d4ada59876408eaab9e5c0229981e381572b5c4f
Neil Crawford <neil.crawford@gmail.com>
2018-12-19 22:56:37 +0000

    6b

---
+++ advent-of-code/2018/day6b.js
@@ -0,0 +1,43 @@
+const { readFileToArray } = require("./utils");
+const _ = require("lodash");
+main();
+
+function readLine(line, index) {
+  const [x, y] = line.split(",").map(i => Number(i));
+  return {
+    x: Number(x),
+    y: Number(y),
+    isInfinite: false,
+    id: index,
+    area: 0,
+    distance: (x1, y1) => Math.abs(x1 - x) + Math.abs(y1 - y)
+  };
+}
+
+async function main() {
+  const points = await readFileToArray("./day6-input.txt", readLine);
+  const matrix = Array(400)
+    .fill([])
+    .map(x => Array(400).fill("."));
+  let area = 0;
+
+  for (let x1 = 0; x1 < 400; x1++) {
+    for (let y1 = 0; y1 < 400; y1++) {
+      if (!points.find(x => x.distance(x1, y1) > 10000)) {
+        matrix[x1][y1] = "#";
+      }
+    }
+  }
+
+  for (let x1 = 0; x1 < 400; x1++) {
+    for (let y1 = 0; y1 < 400; y1++) {
+      if (matrix[x1][y1] === "#") {
+        area++;
+      }
+    }
+  }
+
+  console.log(area);
+
+  process.exit(0);
+}
