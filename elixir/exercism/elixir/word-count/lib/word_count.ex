70c556707a8399a3af537c4ff81abe9dc900937c
Neil Crawford <neil.crawford@gmail.com>
2020-07-05 16:09:34 +0100

    Updating word count solution

--- elixir/exercism/elixir/word-count/lib/word_count.ex
+++ elixir/exercism/elixir/word-count/lib/word_count.ex
@@ -6,5 +6,16 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    cleaned = Regex.replace(~r/[^\p{L}\p{Nd}\-]+/u, sentence, " ")
+
+    cleaned
+    |> String.downcase()
+    |> String.split(" ")
+    |> Enum.filter(fn item -> item !== "" end)
+    |> Enum.reduce(%{}, fn word, acc ->
+      Map.merge(acc, %{word => 1}, fn _k, v1, v2 ->
+        v1 + v2
+      end)
+    end)
   end
 end
