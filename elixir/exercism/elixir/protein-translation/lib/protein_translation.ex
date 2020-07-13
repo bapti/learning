1cc5c74fd7cc4f78a05fc511f3a4630e22ee8ab2
Neil Crawford <neil.crawford@gmail.com>
2020-07-13 21:58:54 +0100

    Adding solution to protein translation

--- elixir/exercism/elixir/protein-translation/lib/protein_translation.ex
+++ elixir/exercism/elixir/protein-translation/lib/protein_translation.ex
@@ -1,9 +1,44 @@
 defmodule ProteinTranslation do
+  @codon_map %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    chunks = for <<x::binary-3 <- rna>>, do: x
+
+    try do
+      s =
+        Enum.map(chunks, &of_codon(&1))
+        |> Enum.map(fn
+          {:ok, str} -> str
+          {:error, _} -> raise "invalid RNA"
+        end)
+        |> Enum.take_while(&(&1 !== "STOP"))
+
+      {:ok, s}
+    rescue
+      RuntimeError -> {:error, "invalid RNA"}
+    end
   end
 
   @doc """
@@ -29,5 +64,9 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    cond do
+      !Map.has_key?(@codon_map, codon) -> {:error, "invalid codon"}
+      true -> {:ok, @codon_map[codon]}
+    end
   end
 end
