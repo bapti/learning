bb2158830c5dfd70a7ba0785d587bf06eedbf340
Neil Crawford <neil.crawford@gmail.com>
2020-07-13 14:06:16 +0100

    Rotational cipher

--- elixir/exercism/elixir/rotational-cipher/mix.exs
+++ elixir/exercism/elixir/rotational-cipher/mix.exs
@@ -7,7 +7,7 @@ defmodule RotationalCipher.MixProject do
       version: "0.1.0",
       # elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
       alias: [test: "test --color"]
     ]
   end
