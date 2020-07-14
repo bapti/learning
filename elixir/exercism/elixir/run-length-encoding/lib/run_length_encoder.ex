ad6f1df6100314f62e45dc01011ab48175947fa4
Neil Crawford <neil.crawford@gmail.com>
2020-07-14 22:37:42 +0100

    Completed run length encoding

--- elixir/exercism/elixir/run-length-encoding/lib/run_length_encoder.ex
+++ elixir/exercism/elixir/run-length-encoding/lib/run_length_encoder.ex
@@ -8,9 +8,49 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t()) :: String.t()
   def encode(string) do
+    string
+    |> String.graphemes()
+    |> Enum.reduce(
+      [],
+      fn
+        letter, [{x, count} | tail] when x == letter ->
+          [{x, count + 1} | tail]
+
+        letter, [{x, count} | tail] when x != letter ->
+          [{letter, 1}, {x, count} | tail]
+
+        letter, [] ->
+          [{letter, 1}]
+      end
+    )
+    |> Enum.reverse()
+    |> Enum.map_join(fn
+      {letter, count} when count == 1 -> letter
+      {letter, count} -> "#{count}#{letter}"
+    end)
   end
 
   @spec decode(String.t()) :: String.t()
   def decode(string) do
+    {decoded, _} =
+      string
+      |> String.graphemes()
+      |> Enum.reduce(
+        {"", ""},
+        fn letter, {acc, num} ->
+          cond do
+            Regex.match?(~r/[0-9]/, letter) ->
+              {acc, num <> letter}
+
+            Regex.match?(~r/[A-Za-z ]/, letter) ->
+              cond do
+                num == "" -> {acc <> letter, ""}
+                true -> {acc <> String.duplicate(letter, String.to_integer(num)), ""}
+              end
+          end
+        end
+      )
+
+    decoded
   end
 end
