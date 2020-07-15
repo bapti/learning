43e845158f5974cce8a3170a44fb71189ad0fa94
Neil Crawford <neil.crawford@gmail.com>
2020-07-15 12:43:08 +0100

    Updated anagram solution

--- elixir/exercism/elixir/anagram/lib/anagram.ex
+++ elixir/exercism/elixir/anagram/lib/anagram.ex
@@ -1,23 +1,20 @@
 defmodule Anagram do
+  defguard are_words_equivalent(word_a, word_b)
+           when String.downcase(word_a) == String.downcase(word_b)
+
   @doc """
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
-  def match(base, candidates) do
-    candidates
-    |> Enum.filter(fn candidate -> is_anagram?(base, candidate) end)
+  def match(base, candidates), do: Enum.filter(candidates, &is_anagram?(base, &1))
+
+  defp is_anagram?(word_a, word_b) do
+    cond do
+      byte_size(word_a) != byte_size(word_b) -> false
+      String.downcase(word_a) == String.downcase(word_b) -> false
+      true -> sorted_letters(word_a) == sorted_letters(word_b)
+    end
   end
 
-  def is_anagram?(str1, str2) do
-    sort_string(str1) == sort_string(str2) and
-      String.downcase(str1) != String.downcase(str2)
-  end
-
-  defp sort_string(str) do
-    str
-    |> String.downcase()
-    |> String.graphemes()
-    |> Enum.sort()
-    |> Enum.join()
-  end
+  defp sorted_letters(word), do: String.downcase(word) |> String.graphemes() |> Enum.sort()
 end
