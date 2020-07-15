c966c5c2c642a8c1828643163b3c84fab47bff54
Neil Crawford <neil.crawford@gmail.com>
2020-07-15 11:51:24 +0100

    Solution for anagram

--- elixir/exercism/elixir/anagram/lib/anagram.ex
+++ elixir/exercism/elixir/anagram/lib/anagram.ex
@@ -4,5 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    candidates
+    |> Enum.filter(fn candidate -> is_anagram?(base, candidate) end)
+  end
+
+  def is_anagram?(str1, str2) do
+    sort_string(str1) == sort_string(str2) and
+      String.downcase(str1) != String.downcase(str2)
+  end
+
+  defp sort_string(str) do
+    str
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.sort()
+    |> Enum.join()
   end
 end
