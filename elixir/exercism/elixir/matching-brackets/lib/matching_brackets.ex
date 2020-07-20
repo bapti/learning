998ec77848781ab747d252c0f41a388971cd8d69
Neil Crawford <neil.crawford@gmail.com>
2020-07-20 10:21:13 +0100

    Brackets solution v2

--- elixir/exercism/elixir/matching-brackets/lib/matching_brackets.ex
+++ elixir/exercism/elixir/matching-brackets/lib/matching_brackets.ex
@@ -10,24 +10,12 @@ defmodule MatchingBrackets do
   end
 
   defp iterate_brackets([], []), do: true
+  defp iterate_brackets(["{" | tail], rest), do: iterate_brackets(tail, ["{" | rest])
+  defp iterate_brackets(["[" | tail], rest), do: iterate_brackets(tail, ["[" | rest])
+  defp iterate_brackets(["(" | tail], rest), do: iterate_brackets(tail, ["(" | rest])
+  defp iterate_brackets(["}" | tail], ["{" | rest]), do: iterate_brackets(tail, rest)
+  defp iterate_brackets(["]" | tail], ["[" | rest]), do: iterate_brackets(tail, rest)
+  defp iterate_brackets([")" | tail], ["(" | rest]), do: iterate_brackets(tail, rest)
   defp iterate_brackets([], _left_brackets), do: false
-  defp iterate_brackets([head | tail], []), do: iterate_brackets(tail, [head])
-
-  defp iterate_brackets([head | tail], [last | rest]) do
-    cond do
-      Regex.match?(~r/[\[\{\(]/, head) ->
-        iterate_brackets(tail, [head, last | rest])
-
-      Regex.match?(~r/[\]\}\)]/, head) and check_pair_match(last, head) ->
-        iterate_brackets(tail, rest)
-
-      true ->
-        false
-    end
-  end
-
-  defp check_pair_match("[", "]"), do: true
-  defp check_pair_match("(", ")"), do: true
-  defp check_pair_match("{", "}"), do: true
-  defp check_pair_match(_, _), do: false
+  defp iterate_brackets(_, _), do: false
 end
